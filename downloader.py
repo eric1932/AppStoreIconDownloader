24e70076eb474fbf0ad5b78e9bbe0fecc8623017
Eric Liu <eric1932777@gmail.com>
2020-05-10 20:50:59 -0500

    Regex functionality improvements

--- downloader.py
+++ downloader.py
@@ -9,7 +9,6 @@ import argparse
 import os
 import re
 import ssl
-import urllib.parse as parse
 import urllib.request as request
 from io import BytesIO
 
@@ -18,45 +17,61 @@ from PIL import Image
 # allow certificate from local issuer
 ssl._create_default_https_context = ssl._create_unverified_context
 
-# Google on the AppStore (test app_url)
+# test app url
+# Google on the AppStore
 app_url = 'https://apps.apple.com/us/app/google/id284815942'
+# app_url = 'https://apps.apple.com/us/app/id284815942'
+# WeChat
+# app_url = 'https://apps.apple.com/us/app/wechat/id414478124'
+# app_url = 'https://apps.apple.com/cn/app/wechat/id414478124'
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Download app icon from App Store')
     parser.add_argument('url', metavar='URL', type=str, nargs='?', help='App Store URL (apps.apple.com...)')
     parser.add_argument('name', type=str, nargs='?', help='Application name (mandatory)')
+    parser.add_argument('--debug', action='store_true')
     args = parser.parse_args()
 
-    # default values
-    download_or_print = True  # True to download; False to print
+    download_or_print = False if args.debug else True  # True to download; False to print
     save_dir = os.path.join(os.path.expanduser("~"), 'Downloads')
 
-    app_url = args.url if args.url else input("App Store URL: ")
+    if args.url:
+        app_url = args.url
+    elif not args.debug:
+        app_url = input("App Store URL: ")
     # match App Store URLs
-    match = re.findall(r"apps\.apple\.com/[a-z]{2}/app/.*/id[0-9]+", app_url)
-    if len(match) == 0:
+    match = re.search(r"(apps\.apple\.com/([a-z]{2})/app/)(.*/)?(id[0-9]+)", app_url)
+    # groups() example output: ('apps.apple.com/us/app/', 'us', 'google/', 'id284815942')
+    if not match:
         print('invalid App Store URL!')
         exit(1)
-    else:
-        # encode app_url & enforce https
-        app_url = r'https://' + parse.quote(match[0])
+    # enforce https
+    app_url = r'https://' + match.group(1) + match.group(4)
+    store_region = match.group(2)
 
     # try reg match
     with request.urlopen(app_url) as response:
-        web_source_code = response.read().decode('UTF-8')
+        web_html = response.read().decode()
     # alternative re match "https:\/\/is.*?-ssl\.mzstatic\.com\/image\/thumb\/.*?AppIcon.*?\.png\/230x0w\.png"
-    image_match = re.findall(r"https:\/\/is.*?-ssl\.mzstatic\.com\/image\/thumb\/.*?\.png\/230x0w\.png",
-                             web_source_code)
+    image_match = re.findall(r"https:\/\/is.*?-ssl\.mzstatic\.com\/image\/thumb\/.*?\.png\/230x0w\.png", web_html)
     if len(image_match) == 0:
         print('no matches found!')
         exit(1)
     else:
         print("found image url!")
-    version_match = re.findall(r"<p class=\"l-column small-6 medium-12 whats-new__latest__version\">.*?</p>",
-                               web_source_code)  # returns a list
-    version = "NA"
-    if len(version_match) != 0:
-        version = version_match[0][65:][:-4]  # extract version info
+
+    # Get app name
+    if store_region == 'cn':
+        app_name = re.search("<title>‎App\xa0Store 上的“(.*)”</title>", web_html).group(1)
+    else:
+        app_name = re.search("<title>\u200e(.*) on the App\xa0Store</title>", web_html).group(1)
+
+    # Get app version
+    # '<p class="l-column small-6 medium-12 whats-new__latest__version">Version 105.0</p>'
+    if store_region == 'cn':
+        app_version = re.search(r"whats-new__latest__version\">版本 (.*?)</p>", web_html).group(1)
+    else:
+        app_version = re.search(r"whats-new__latest__version\">Version (.*?)</p>", web_html).group(1)
 
     image_url_orig = image_match[0]
     # make sure to get largest icon size
@@ -66,13 +81,16 @@ if __name__ == '__main__':
     print('image size is: {0}'.format(img.size))
     image_url_max = re.sub(r'10240x0w', str(img.size[0]) + 'x0w', image_url_10240)
     if download_or_print:
-        app_name = args.name if args.name else input("input App name: ")
-        output_file_name = app_name + '_' + version + '_' + str(img.size[0]) + 'x0w.png'
+        if args.name:
+            app_name = args.name
+        output_file_name = app_name + '_' + app_version + '_' + str(img.size[0]) + 'x0w.png'
         print('saving image to file \"' + output_file_name + '\"')
         with request.urlopen(image_url_max) as response, open(os.path.join(save_dir, output_file_name), 'wb') as file:
             img_bin = response.read()
             file.write(img_bin)
         print('success!')
     else:
-        print('image url:')
-        print(image_url_max)
+        print('app name:', app_name)
+        print('version:', app_version)
+        print('store url:', app_url)
+        print('image url:', image_url_max)
