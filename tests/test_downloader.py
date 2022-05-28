7d2a07d7813054f405bb2820a095a4ebc7059adf
Eric Liu <eric1932777@gmail.com>
2022-05-28 00:08:24 -0500

    Update test cases

--- tests/test_downloader.py
+++ tests/test_downloader.py
@@ -13,9 +13,14 @@ class DownloaderTestCase(unittest.TestCase):
                        save_path='./')
         contain_google = list(filter(lambda x: "google" in x.lower(), os.listdir()))
         self.assertTrue(len(contain_google) > 0)
-        self.assertIsNotNone(re.match(r"Google_[0-9.]+_[0-9]+x0w.(jpg|png)", contain_google[0]))
-        # cleanup
-        os.remove(contain_google[0])
+        try:
+            self.assertIsNotNone(re.match(r"Google_[0-9.]+_[0-9]+x0w.(jpeg|png|webp)", contain_google[0]))
+        except AssertionError as e:
+            print(contain_google)
+            raise e
+        finally:
+            # cleanup
+            os.remove(contain_google[0])
 
     def test_download_staples(self):
         download_image(APP_STORE_US_STAPLES,
@@ -23,9 +28,14 @@ class DownloaderTestCase(unittest.TestCase):
                        save_path='./')
         contain_staples = list(filter(lambda x: "staples" in x.lower(), os.listdir()))
         self.assertTrue(len(contain_staples) > 0)
-        self.assertIsNotNone(re.match(r"Staples.*?_[0-9.]+_[0-9]+x0w.(jpg|png)", contain_staples[0]))
-        # cleanup
-        os.remove(contain_staples[0])
+        try:
+            self.assertIsNotNone(re.match(r"Staples.*?_[0-9.]+_[0-9]+x0w.(jpeg|png|webp)", contain_staples[0]))
+        except AssertionError as e:
+            print(contain_staples)
+            raise e
+        finally:
+            # cleanup
+            os.remove(contain_staples[0])
 
     # TODO test 404 page
 
